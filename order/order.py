f9b46d970f481cb29ab9d65d9edb92010e2a14a0
Will Plusnick <pwplusni@us.ibm.com>
2016-01-05 19:55:46 -0600

    Improved code structure
    I created a superclass called OrderType that takes care of the common
    tasks between different configurations and hardware components.

--- order/order.py
+++ order/order.py
@@ -1,17 +1,39 @@
 import SoftLayer as sl
 import yaml, logging, os, fnmatch
 
-class Node:
-    _required_definitions = ['name']
-    def __init__(self, node_definition):
+hw_nicks = {}
+
+class OrderType:
+    """ The Grand class that will be the base class for all orders
+    It has definitions and keywords and makes sure that the definition meets the minimum requirements
+    and only uses keywords."""
+    _required_definitions = []
+    _keywords = []
+    def __init__(self, definition, nicks = {}):
+        self.definition = definition
+
+    def check_requirements(self):
+        # make sure it has required definitions
         for requirement in self._required_definitions:
-            try:
-                node_definition[requirement]
-            except KeyError:
-                logging.critical("Node definition %s does not have required field %s\n"
-                                 "Node definition passed: %s \n"
-                                 "Exiting!" % (self.name, requirement, node_definition))
+            if requirement not in self.definition.keys():
+                logging.critical("%s definition %s does not have required field %s\n"
+                                 "Exiting!" % (self.__class__.__name__, self.definition, requirement))
                 exit(1)
+    def check_keywords(self):
+        for arg in self.definition.keys():
+            if arg not in self._keywords:
+                logging.critical("%s definition %s has no keyword called %s."
+                                 "Exiting!" % (self.__class__.__name__, self.definition, requirement))
+                exit(1)
+
+class Node (OrderType):
+    """ A node defines a name that is assigned to a particular hardware configurations """
+    _required_definitions = ['name', 'cpu']
+    _keywords = []
+    def __init__(self, node_definition):
+        OrderType.__init__(self, node_definition)
+        self.check_requirements()
+        self.check_keywords
         self.name = node_definition['name']
         self.node = node_definition
     def __repr__(self):
@@ -21,25 +43,15 @@ class Node:
         return rep
 
 class Environment:
-    _required_definitions = []
+    """ Environment defines a particular configuration of nodes """
+    _required_definitions = ['node']
     def __init__(self, environment_definition, node_types):
-        self.name = name
         self.env = []
-        for node in environment_definition:
-            # make sure node has required definitions
-            for requirement in self._required_definitions:
-                try:
-                    node[requirement]
-                except KeyError:
-                    logging.critical("Environment definition %s does not have required field %s\n"
-                                     "Environment definition passed: %s\n"
-                                     "Exiting!" % (self.name, requirement, environment_definition))
-                    exit(1)
+        self.keywords = self.keywords + self.required_keywords + node_types.keys()
+        OrderType.__init__(self, environment_definition)
+        self.check_requirements()
+        self.check_arguments()
 
-            if node['node'] not in node_types.keys():
-                logging.error("No node definition for not type: %s\n"
-                              "Exiting!" % node['node'])
-                exit(1)
     def __repr__(self):
         rep = "%s\n" % self.name
         for node in self.env:
@@ -48,6 +60,8 @@ class Environment:
         return rep
 
 def parser_generator(ftype, obj = None):
+    """ Generate a function that will parse the yaml files with a specificied name convention
+    and returns a list of a specified object of parsed files """
     def parser(dir, object_dict = {}):
         file_list = [file for file in os.listdir(dir) if fnmatch.fnmatch(file, ftype)]
         for file in file_list:
@@ -60,11 +74,16 @@ def parser_generator(ftype, obj = None):
             else:
                 object_dict = object_list
         if not file_list:
-            logging.warn('No %s definition files in %s!' % (ftype, dir))
+            logging.info('No %s definition files in %s!' % (ftype, dir))
         return object_dict
     return parser
 
 if __name__ == '__main__':
+
+    # find all hardware nicknames
+    hw_nick_parser = parser_generator('*.hwn', None)
+    hw_nicks = hw_nick_parser('definitions/std')
+    hw_nicks = hw_nick_parser('definitions/usr', object_dict = hw_nicks)
     
     # find all node definition files
     node_parser = parser_generator('*.node', Node)
@@ -75,7 +94,8 @@ if __name__ == '__main__':
 #    env_types = env_parser('definitions/std')
 #    env_types = env_parser('definitions/usr', object_dict = env_types)
 
-    for node in node_types:
-        print node
+    print hw_nicks
+    print node_types
+    print node_types['FooBar']
 #    for env in env_types:
 #        print env
