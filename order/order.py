9889713e5de2bf1809effca9ff568df8d938cf2f
Will Plusnick <pwplusni@us.ibm.com>
2016-01-07 19:40:37 -0600

    Much expansion.

--- order/order.py
+++ order/order.py
@@ -1,7 +1,12 @@
 import SoftLayer as sl
 import yaml, logging, os, fnmatch
 
+
 hw_nicks = {}
+
+def identity(arg):
+    "Returns its argument"
+    return arg
 
 class OrderType:
     """ The Grand class that will be the base class for all orders
@@ -9,57 +14,113 @@ class OrderType:
     and only uses keywords."""
     _required_definitions = []
     _keywords = []
+    _keyword_handler = {}
     def __init__(self, definition, nicks = {}):
         self.definition = definition
+        self._keywords = self._keywords + self._required_definitions
+        self._order = {}
 
-    def check_requirements(self):
+        self._check_requirements()
+        self._check_keywords()
+        self._parse()
+
+    def __getitem__(self, index):
+        try:
+            return self._order[index]
+        except KeyError:
+            logging.critical("%s definition %s has no field %s\n"
+                             "Exiting!" % (self.__class__.__name__, self.definition, index))
+
+    def __repr__(self):
+        return self.__class__.__name__
+
+    def _check_requirements(self):
         # make sure it has required definitions
         for requirement in self._required_definitions:
             if requirement not in self.definition.keys():
                 logging.critical("%s definition %s does not have required field %s\n"
                                  "Exiting!" % (self.__class__.__name__, self.definition, requirement))
                 exit(1)
-    def check_keywords(self):
-        for arg in self.definition.keys():
-            if arg not in self._keywords:
-                logging.critical("%s definition %s has no keyword called %s."
-                                 "Exiting!" % (self.__class__.__name__, self.definition, requirement))
+
+    def _check_keywords(self):
+        # check to make sure every field is both defined and has a handler
+        for field in self.definition.keys():
+            if field not in self._keywords:
+                logging.critical("%s definition %s has no keyword called %s.\n"
+                                 "Exiting!" % (self.__class__.__name__, self.definition, field))
                 exit(1)
+        for keyword in self._keywords:
+            if keyword not in self._keyword_handler.keys():
+                logging.critical("%s keyword %s has no handler."
+                                 "Exiting!" % (self.__class__.__name__, keyword))
+                exit(1)
+
+
+    def _parse(self):
+        for field in self.definition.keys():
+            self._order[field] = self._keyword_handler[field](self.definition[field])
 
 class Node (OrderType):
     """ A node defines a name that is assigned to a particular hardware configurations """
     _required_definitions = ['name', 'cpu']
-    _keywords = []
+    _keywords = ['hd']
+    _keyword_handler = \
+        {
+        'name': identity,
+        'cpu': identity,
+        'hd': identity
+        }
+
     def __init__(self, node_definition):
         OrderType.__init__(self, node_definition)
-        self.check_requirements()
-        self.check_keywords
-        self.name = node_definition['name']
-        self.node = node_definition
+
     def __repr__(self):
-        rep = "%s\n" % self.name
-        for spec in sorted(self.node.keys(), key=str.lower):
-            rep = rep + "\t%r:\t%r\n" % (spec, self.node[spec])
+        rep = ("%s\n" 
+               "%s\n" % (OrderType.__repr__(self), self['name']))
+        for spec in sorted(self._order.keys(), key=str.lower):
+            rep = rep + "\t%r:\t%r\n" % (spec, self._order[spec])
         return rep
 
-class Environment:
+class Environment (OrderType):
     """ Environment defines a particular configuration of nodes """
-    _required_definitions = ['node']
-    def __init__(self, environment_definition, node_types):
-        self.env = []
-        self.keywords = self.keywords + self.required_keywords + node_types.keys()
+    _required_definitions = ['nodes', 'name']
+    _keywords = []
+    _keyword_handler = {'name': identity}
+
+    def __init__(self, environment_definition, node_types={}):
+        self.node_types = node_types
+        self._env = {}
+        self._keyword_handler['nodes'] = self._parse_nodes
+
         OrderType.__init__(self, environment_definition)
-        self.check_requirements()
-        self.check_arguments()
+        self._check_requirements()
+        self._check_keywords()
 
     def __repr__(self):
-        rep = "%s\n" % self.name
-        for node in self.env:
-            rep = rep + ("\t%r\n" 
-                         "-------------------\n" % node)
+        rep =("%r\n"
+              "%s\n" % (OrderType.__repr__(self), self['name']))
+        for name, node in self._env.iteritems():
+            rep = rep + ("\t%r: %r\n" 
+                         "-------------------\n" % (name, node))
         return rep
 
-def parser_generator(ftype, obj = None):
+    def _parse(self):
+        OrderType._parse(self)
+        for name, node in self._order.iteritems():
+            if name not in self._keywords:
+                self._env[name] = node
+
+    def _parse_nodes(self, nodes):
+        for name, num in nodes.iteritems():
+            try:
+                self._order[name] = {'node': self.node_types[name], 'num': num}
+            except KeyError:
+                logging.critical("%s calls non-existant node %s"
+                                 % (self.__class__.__name__,
+                                    name))
+                exit(1)
+
+def yaml_parser_generator(ftype, obj = None, **kwargs):
     """ Generate a function that will parse the yaml files with a specificied name convention
     and returns a list of a specified object of parsed files """
     def parser(dir, object_dict = {}):
@@ -69,8 +130,8 @@ def parser_generator(ftype, obj = None):
                 object_list = yaml.safe_load(f)
             if obj is not None:
                 for object in object_list:
-                    object = obj(object)
-                    object_dict[object.name] = object
+                    object = obj(object, **kwargs)
+                    object_dict[object['name']] = object
             else:
                 object_dict = object_list
         if not file_list:
@@ -81,21 +142,20 @@ def parser_generator(ftype, obj = None):
 if __name__ == '__main__':
 
     # find all hardware nicknames
-    hw_nick_parser = parser_generator('*.hwn', None)
+    hw_nick_parser = yaml_parser_generator('*.hwn', None)
     hw_nicks = hw_nick_parser('definitions/std')
     hw_nicks = hw_nick_parser('definitions/usr', object_dict = hw_nicks)
     
     # find all node definition files
-    node_parser = parser_generator('*.node', Node)
+    node_parser = yaml_parser_generator('*.node', Node)
     node_types = node_parser('definitions/std')
     node_types = node_parser('definitions/usr', object_dict = node_types)
     # find all environment definition files
-#    env_parser = parser_generator('*.env', Environment)
-#    env_types = env_parser('definitions/std')
-#    env_types = env_parser('definitions/usr', object_dict = env_types)
+    env_parser = yaml_parser_generator('*.env', Environment, node_types=node_types)
+    env_types = env_parser('definitions/std')
+    env_types = env_parser('definitions/usr', object_dict = env_types)
 
-    print hw_nicks
-    print node_types
-    print node_types['FooBar']
-#    for env in env_types:
-#        print env
+#    print hw_nicks
+#    print node_types
+#    print node_types['FooBar']['cpu']
+    print env_types
